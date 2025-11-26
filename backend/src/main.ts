addd14cdad758e3c844ed5aad7621b5707792d81
Lautaro Caballero <caballerolautarodev@gmail.com>
2025-11-26 07:50:21 -0300

    🔧 Refine CORS settings in main.ts: Expand allowed origins, specify methods and headers, and adjust preflight options for enhanced security and flexibility in API access.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,8 +5,16 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://clinical-azure.vercel.app/'],
+    origin: [
+      'https://clinical-azure.vercel.app', // CAMBIAR por tu dominio real
+      'https://*.vercel.app',
+      'http://localhost:3000',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
+    allowedHeaders: 'Content-Type, Authorization',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
   });
 
   const port = process.env.PORT || 3001;
